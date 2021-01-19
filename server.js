8091cb21abd16a4b6f5c04925eaa18eec29a5fb3
Steven <stevencschoi@gmail.com>
2021-01-18 23:24:41 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv");
+
+const PORT = process.env.PORT || 5000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('We out here!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+})
